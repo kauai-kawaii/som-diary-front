ba1857e223e7e5e7424f5068afd9decd162ff69f
fbgjung <fbgjung@gmail.com>
2024-05-05 17:44:50 +0900

    feat: 수정용 이미지 업로드, 날씨 업로드 컴포넌트 구현

---
+++ src/components/diary/EditImageUpload.js
@@ -0,0 +1,80 @@
+import { PhotoIcon} from '@heroicons/react/24/solid'
+import { useState,useEffect } from 'react';
+
+export default function EditImageUpload({ onImageChange, savedImage }){
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0]; 
+
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setSelectedImage(reader.result);
+                onImageChange(file);
+            };
+            reader.readAsDataURL(file);
+        } else {
+            setSelectedImage(null);
+        }
+    };   
+
+    useEffect(() => {
+        if (savedImage) {
+            setSelectedImage(savedImage);
+        } else {
+            console.log("erroer")
+        }
+    }, []);
+
+    const handleDeleteImage =() => {
+        setSelectedImage(null);
+    }
+
+    return(
+        <div className="mt-3 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+            <div className="text-center">
+                {selectedImage ? (
+                    <div>
+                        <img className="inputImage" src={selectedImage} alt="Selected"
+                             style={{ objectFit:'contain' }}/>
+                        <div className="mt-4 flex text-sm leading-6 text-gray-600 justify-center">
+                            <label
+                                htmlFor="file-upload"
+                                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                            >
+                                <span>수정하기</span>
+                                <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={handleImageChange} />
+
+                            </label>
+                        </div>
+
+                        {/* 이미지 있을 때 삭제하는 버튼 */}
+                        <div className="mt-4 flex text-sm leading-6 text-gray-600 justify-center">
+                            <button
+                                className="relative cursor-pointer rounded-md bg-white font-semibold text-gray-400 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                                onClick={handleDeleteImage}
+                            >
+                                <span>삭제</span>
+                            </button>
+                        </div>
+                    </div>
+
+                ) : (
+                    <>
+                        <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
+                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
+                            <label
+                                htmlFor="file-upload"
+                                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                            >
+                                <span>사진 업로드하기</span>
+                                <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={handleImageChange} />
+                            </label>
+                        </div>
+                    </>
+                )}
+            </div>
+        </div>
+    )
+}
---
+++ src/components/diary/EditWeatherUpload.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+export default function WeatherUpload({ data, setWeatherData,savedWeather }) {
+    const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    // const [weatherInfo, setWeatherInfo] = useState({
+    //     temperature: null,
+    //     description: ""
+    // });
+    const [weatherInfo, setWeatherInfo] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if (savedWeather) {
+            setWeatherInfo(savedWeather);
+        } else {
+            console.log("erroer")
+        }
+
+        if (data) {
+            const latitude = data.y;
+            const longitude = data.x;
+
+            const getWeather = async (lat, lon) => {
+                try {
+                    const response = await fetch(
+                        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric&lang=kr`
+                    );
+
+                    if (!response.ok) {
+                        throw new Error('날씨 정보 없음');
+                    }
+
+                    const json = await response.json();
+                    setWeatherInfo(json.main.temp);
+                } catch (error) {
+                    setError(error.message);
+                }
+            };
+
+            getWeather(latitude, longitude);
+        }
+    }, []);
+
+    useEffect(() => {
+        // 날씨 정보가 변경될 때마다 Diary 컴포넌트로 업데이트된 날씨 정보 전달
+        setWeatherData(weatherInfo);
+    }, [weatherInfo, setWeatherData]);
+
+    return (
+        <div className="sm:col-span-2">
+            <div className="mt-2">
+                <div className="block w-full rounded-md border-0 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                    {error ? (
+                        <p className="text-center">{error}</p>
+                    ) : (
+                        <p className="text-center">{weatherInfo !== null ? `${weatherInfo}°C ` : '날씨 정보 없음'}</p>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
